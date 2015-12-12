94dfb974a6f9e9fc89efad27fdd48069d1762fa8
bernardo-vieira256 <blvieira5@gmail.com>
2015-12-12 18:18:21 +0000

    Alteracao 0b1
    Turnos,Eventos,etc

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <ctime>
 
 using std::cout;
 using std::endl;
@@ -12,37 +13,94 @@ using std::vector;
 #include "nave.h"
 #include "unidades.h"
 
+void ChuvaMeteroritos(Nave& nave) {
+
+
+
+}
 
 int main(void) {
 
-	string nome;
-	int dificuldade, m_percorridas = 0;
+	string nome, comando;
+	int dificuldade, m_percorridas = 0, p_evento = 0, t_turnos = 0;
 
 	cout << "Introduza um nome para a sua nave: " << endl;
 	getline(cin, nome);
 
-	cout << "Nível de dificuldade da missão?" << endl;
+	cout << "Nivel de dificuldade da missao?" << endl;
 	cin >> dificuldade;
 
 	Nave nave(nome);
-	//nave.ConfigurarSala(0, 0, new Propulsor());
 
+	//preparar a viagem
+	//definir salas opcionais
+
+	//definir tripulacao
+
+	std::srand((unsigned int)std::time(0));
 	while (1) {
-		
-		if( (4000 + 1000)*dificuldade >= m_percorridas){
-			
+		cout << ((4000 + 1000)*dificuldade - m_percorridas) <<
+			" milhas ate ao fim!" << endl;
+
+		if( (4000 + 1000)*dificuldade <= m_percorridas){
+			cout << "Fim da brincadeira!" << endl << "Voce ganhou" << endl;
+			break;
 		}
 		//Inicio da turno
+		//repirar
+		/*for (int l = 0; l < 3; l++) {
+			for (int c = 0; c < 5; c++) {
+				
+				vector<Unidades*> unidades = nave.UnidadesNaSala(l, c);
+
+				for (std::vector<Unidades*>::iterator it = unidades.begin();
+				it < unidades.end(); it++) {
+					//(*it)->PerdeVida();
+				}
+
+
+			}
+		}*/
 
 		//Fase de ordens
 
-		//Final do turno
-		for (int l = 0; l < 3; l++) {
-			for (int c = 0; c < 5; c++) {
-			}
+		while (1) {
+			cout << "Introduza comando para nova ordem:";
+			//cin.ignore();
+			getline(cin, comando);
+			if (comando == "sair") break;
 		}
 
+		//Final do turno
+		//adicionar oxigenio ás salas
+		//recuperar escudo da sala de controlo de escudo
+
+		m_percorridas += nave.mPercorrer();//verificar se alguem esta na Ponte
+
 		//Eventos
+		if (!t_turnos || t_turnos == p_evento) {
+			switch (rand() % 4)
+			{
+			case 0:
+				//
+				ChuvaMeteroritos(nave);
+				break;
+			case 1:
+				//
+				break;
+			case 2:
+				//
+				break;
+			case 3:
+				//
+				break;
+			}
+			//evento
+			cout << "Evento" << endl;
+
+			p_evento += 5 + std::rand() % 5;
+		}
+		t_turnos++;
 	}
 
 
--- unidades.cpp
+++ unidades.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
 using std::vector;
+using std::string;
 
 #include "caract.h"
 #include "unidades.h"
 #include "sala.h"
 
 Unidades::Unidades(int max) : MAX_PV(max), PV(max) { }
+
+void Unidades::GanhaVida()
+{
+}
